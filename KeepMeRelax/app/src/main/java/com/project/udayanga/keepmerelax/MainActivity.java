0ea9d7ad1abe5ce433c69f1cdc334a8476d90ce7
Udayanga Senanayake <u.senanayake91@gmail.com>
2016-03-27 14:48:39 +0530

    Downloaded sample application & sample icons

--- KeepMeRelax/app/src/main/java/com/project/udayanga/keepmerelax/MainActivity.java
+++ KeepMeRelax/app/src/main/java/com/project/udayanga/keepmerelax/MainActivity.java
@@ -1,5 +1,6 @@
 package com.project.udayanga.keepmerelax;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,8 +81,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.add_relation) {
+
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
