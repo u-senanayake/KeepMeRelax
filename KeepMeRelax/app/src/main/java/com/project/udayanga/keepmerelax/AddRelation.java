3badf37133c30b0a95050f40b71795066e765622
Udayanga Senanayake <u.senanayake91@gmail.com>
2016-03-27 11:46:53 +0530

    first commit

---
+++ KeepMeRelax/app/src/main/java/com/project/udayanga/keepmerelax/AddRelation.java
@@ -0,0 +1,13 @@
+package com.project.udayanga.keepmerelax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRelation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_relation);
+    }
+}
